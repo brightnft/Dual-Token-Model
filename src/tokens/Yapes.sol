b3ecf76d5541b4813ef3bd02072aa0d5ebb289ae
Trong Dinh Thai Hoang <trongdth@gmail.com>
2024-08-01 13:05:25 +0700

    update yapes and yoints token

--- src/tokens/Yapes.sol
+++ src/tokens/Yapes.sol
@@ -1,77 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import { IPrevention } from "../BP/interfaces/IPrevention.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 // Note: Since the contract has activeTrading disabled by default, we can remove blacklisting functionality
 
-/// @title Yapes Token ($YAPES)
+/// @title Yapes Token ($YOINTS)
 /// @notice ERC-20 Token with management features for trading control, overflow handling, and administrative actions
 /// @dev Extends OpenZeppelin's ERC20, Ownable
-contract Yapes is ERC20, Ownable, ReentrancyGuard {
-  mapping(address => bool) private _blacklisted;
-
-  bool public activeTrading = false;
+contract Yapes is ERC20, Ownable {
+  address public constant BURN_ADDRESS = 0x000000000000000000000000000000000000dEaD;
+  address public bpAddr;
+  bool public isEnableBP;
 
   // 100 million tokens with 18 decimals
-  uint256 public constant INITIAL_SUPPLY = 21000000 * (10 ** 18);
+  uint256 public constant INITIAL_SUPPLY = 100_000_000 * (10 ** 18);
 
-  /// @dev Marks the timestamp when the contract is deployed
-  uint256 public immutable deploymentTime;
-
-  /// @notice Constructs the Yapes Token
-  constructor() ERC20("YAPES Token", "YAPES") Ownable(_msgSender()) {
+  /// @notice Constructs the Yoints Token
+  constructor(address bpAddr_) ERC20("YOINTS Token", "YOINTS") Ownable(_msgSender()) {
     _mint(_msgSender(), INITIAL_SUPPLY);
-    deploymentTime = block.timestamp;
+    bpAddr = bpAddr_;
+    isEnableBP = true;
   }
 
-  /// @notice Modifier to check if trading is allowed
-  /// @dev Can only be called by the owner or if trading is allowed
-  modifier tradeControl() {
-    require(
-      _msgSender() == owner() || (activeTrading && !_blacklisted[_msgSender()]),
-      "Account is blacklisted or trading is disabled"
-    );
-    _;
+  /// @notice Function to enable/disable blacklist prevention
+  function setEnableBP(bool isEnableBP_) external onlyOwner {
+    isEnableBP = isEnableBP_;
   }
 
-  /// @notice Enables active trading of the token
-  /// @dev Can only be enabled by the owner
-  function enableActiveTrading() external onlyOwner {
-    activeTrading = true;
-  }
-
-  /// @notice Blacklists an address to prevent trading
-  /// @param account Address to be blacklisted
-  /// @dev Blacklisting is only possible within 72 hours of contract deployment
-  function blockAccount(address account) external onlyOwner {
-    require(block.timestamp <= deploymentTime + 72 hours, "Blacklisting period expired");
-    _blacklisted[account] = true;
-  }
-
-  /// @notice Unblacklists an address to allow trading
-  /// @param account Address to be unblacklisted
-  function unblockAccount(address account) external onlyOwner {
-    _blacklisted[account] = false;
+  /// @notice Function to update blacklist prevention address
+  function setBPAddress(address bpAddr_) external onlyOwner {
+    bpAddr = bpAddr_;
   }
 
   /// @notice Allows recovery of ERC-20 tokens sent to this contract by mistake
-  /// @param tokenAddress Address of the ERC-20 token to recover
   /// @param tokenAmount Amount of tokens to recover
   /// @dev Can only be called by the owner
-  function claimOverflow(address tokenAddress, uint256 tokenAmount) external onlyOwner nonReentrant {
-    ERC20 token = ERC20(tokenAddress);
-    require(token.transfer(_msgSender(), tokenAmount), "Transfer failed");
+  function claimOverflow(uint256 tokenAmount) external onlyOwner {
+    super._transfer(address(this), _msgSender(), tokenAmount);
   }
 
   /// @notice Overrides the transfer function to include trading controls
   /// @param recipient Recipient of the tokens
   /// @param amount Amount of tokens to transfer
   /// @return A boolean that indicates if the operation was successful.
-  function transfer(address recipient, uint256 amount) public override tradeControl returns (bool) {
-    return super.transfer(recipient, amount);
+  function transfer(address recipient, uint256 amount) public override returns (bool) {
+    _transferToken(_msgSender(), recipient, amount);
+    return true;
   }
 
   /// @notice Overrides the transferFrom function to include trading controls
@@ -79,7 +56,32 @@ contract Yapes is ERC20, Ownable, ReentrancyGuard {
   /// @param recipient Recipient of the tokens
   /// @param amount Amount of tokens to be transferred
   /// @return A boolean that indicates if the operation was successful.
-  function transferFrom(address sender, address recipient, uint256 amount) public override tradeControl returns (bool) {
-    return super.transferFrom(sender, recipient, amount);
+  function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+    uint256 currentAllowance = allowance(sender, _msgSender());
+    require(currentAllowance >= amount, "exceed allowance");
+    unchecked {
+      _approve(sender, _msgSender(), currentAllowance - amount);
+    }
+
+    _transferToken(sender, recipient, amount);
+    return true;
+  }
+
+  /// @dev overrides transfer function to meet tokenomics
+  function _transferToken(address sender, address recipient, uint256 amount) internal {
+    require(amount > 0, "amount is zero");
+
+    if (recipient == BURN_ADDRESS) {
+      // Burn all the amount
+      super._burn(sender, amount);
+      return;
+    }
+
+    if (isEnableBP && bpAddr != address(0x0)) {
+      IPrevention(bpAddr).protect(sender, recipient, amount);
+    }
+
+    // Transfer all the amount
+    _transfer(sender, recipient, amount);
   }
 }
--- src/tokens/Yoints.sol
+++ src/tokens/Yoints.sol
@@ -1,87 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import { IPrevention } from "../BP/interfaces/IPrevention.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-
-// Note: Since the contract has activeTrading disabled by default, we can remove blacklisting functionality
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 /// @title Yoints Token ($YOINTS)
 /// @notice ERC-20 Token with management features for trading control, overflow handling, and administrative actions
 /// @dev Extends OpenZeppelin's ERC20, Ownable
-contract Yoints is ERC20, Ownable {
-  address public constant BURN_ADDRESS = 0x000000000000000000000000000000000000dEaD;
-  address public bpAddr;
-  bool public isEnableBP;
-
-  // 100 million tokens with 18 decimals
-  uint256 public constant INITIAL_SUPPLY = 100_000_000 * (10 ** 18);
-
-  /// @notice Constructs the Yoints Token
-  constructor(address bpAddr_) ERC20("YOINTS Token", "YOINTS") Ownable(_msgSender()) {
-    _mint(_msgSender(), INITIAL_SUPPLY);
-    bpAddr = bpAddr_;
-    isEnableBP = true;
+contract Yoints is ERC20Votes, ERC20Permit, Ownable {
+  constructor() ERC20("Yoints Token", "YOINTS") ERC20Permit("Yoints Token") Ownable(_msgSender()) {
+    _mint(_msgSender(), 100_000_000 * 10 ** decimals());
   }
 
-  /// @notice Function to enable/disable blacklist prevention
-  function setEnableBP(bool isEnableBP_) external onlyOwner {
-    isEnableBP = isEnableBP_;
+  function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+    return super.nonces(owner);
   }
 
-  /// @notice Function to update blacklist prevention address
-  function setBPAddress(address bpAddr_) external onlyOwner {
-    bpAddr = bpAddr_;
+  function _update(address from, address to, uint256 value) internal virtual override(ERC20, ERC20Votes) {
+    super._update(from, to, value);
   }
 
-  /// @notice Allows recovery of ERC-20 tokens sent to this contract by mistake
-  /// @param tokenAmount Amount of tokens to recover
-  /// @dev Can only be called by the owner
-  function claimOverflow(uint256 tokenAmount) external onlyOwner {
-    super._transfer(address(this), _msgSender(), tokenAmount);
+  function mint(address to, uint256 amount) external onlyOwner {
+    super._mint(to, amount);
   }
 
-  /// @notice Overrides the transfer function to include trading controls
-  /// @param recipient Recipient of the tokens
-  /// @param amount Amount of tokens to transfer
-  /// @return A boolean that indicates if the operation was successful.
-  function transfer(address recipient, uint256 amount) public override returns (bool) {
-    _transferToken(_msgSender(), recipient, amount);
-    return true;
-  }
-
-  /// @notice Overrides the transferFrom function to include trading controls
-  /// @param sender Source of the tokens
-  /// @param recipient Recipient of the tokens
-  /// @param amount Amount of tokens to be transferred
-  /// @return A boolean that indicates if the operation was successful.
-  function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-    uint256 currentAllowance = allowance(sender, _msgSender());
-    require(currentAllowance >= amount, "exceed allowance");
-    unchecked {
-      _approve(sender, _msgSender(), currentAllowance - amount);
-    }
-
-    _transferToken(sender, recipient, amount);
-    return true;
-  }
-
-  /// @dev overrides transfer function to meet tokenomics
-  function _transferToken(address sender, address recipient, uint256 amount) internal {
-    require(amount > 0, "amount is zero");
-
-    if (recipient == BURN_ADDRESS) {
-      // Burn all the amount
-      super._burn(sender, amount);
-      return;
-    }
-
-    if (isEnableBP && bpAddr != address(0x0)) {
-      IPrevention(bpAddr).protect(sender, recipient, amount);
-    }
-
-    // Transfer all the amount
-    _transfer(sender, recipient, amount);
+  function burn(address from, uint256 amount) external onlyOwner {
+    super._burn(from, amount);
   }
 }
